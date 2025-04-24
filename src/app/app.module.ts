9d1c1607c49cf3afad0a326be07b26ef827910fd
Ritchie <ritchiedorado@gmail.com>
2025-04-24 20:27:25 +0800

    niggatron

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { TypographyComponent } from './typography/typography.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ComponentsModule,
+    RouterModule,
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    TypographyComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/typography/typography.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typography',
+  templateUrl: './typography.component.html',
+  styleUrls: ['./typography.component.css']
+})
+export class TypographyComponent implements OnInit {
+handleEvent(arg0: string,arg1: any) {
+throw new Error('Method not implemented.');
+}
+viewDate: any;
+events: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
